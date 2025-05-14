56ffab8d46f9b021162fa7eff49697658983c83e
ankit.kumar16 <ankit.kumar16@WKSPUN05GTR6002.corp.mphasis.com>
2025-05-14 12:37:26 +0530

    Files Added

---
+++ BillingSystem/src/com/ankit/billing/BillingCalculator.java
@@ -0,0 +1,16 @@
+package com.ankit.billing;
+
+public class BillingCalculator {
+	public static void main(String[] args) {
+		int units = 299;
+		double billToPay = 0;
+		if(units < 100) {
+			billToPay = units*1.20;
+		}
+		else if(units < 300) {
+			billToPay = 100*1.20+(units-100)*2;
+		}
+		System.out.println("The electricity bill for " + units+ " is: " + billToPay);
+	}
+
+}
